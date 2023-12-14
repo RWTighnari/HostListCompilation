ade5d8d033f3d60873a7dd881c9661cc945d45c2
moon <134622706+RWTighnari@users.noreply.github.com>
2023-12-14 19:53:01 +0800

    Update sed.sh
    Fix --->
    sed: can't read temp0.txt: No such file or directory
    Error: Process completed with exit code 2.

--- src/script/sed.sh
+++ src/script/sed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed '/izat/d;/xtra/d;/beacon/d;/otheve/d;/click/d;/collect/d;/connect/d;/consent/d;/contact/d;/content/d;/cookie/d;/market/d;/trac/d;/analy/d;/adservice/d;/metric/d;/measure/d;/telemetr/d;/||dns\./d;/||doh\./d;/||dot\./d' temp0.txt > temp1.txt
+sed '/izat/d;/xtra/d;/beacon/d;/otheve/d;/click/d;/collect/d;/connect/d;/consent/d;/contact/d;/content/d;/cookie/d;/market/d;/trac/d;/analy/d;/adservice/d;/metric/d;/measure/d;/telemetr/d;/||dns\./d;/||doh\./d;/||dot\./d' output/DNSRules/temp0.txt > output/DNSRules/temp1.txt
